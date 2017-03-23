9936cc7d3729146cf2302162985ec7080db99107
developer <dev@sandbox>
2017-03-22 21:04:06 -0400

    added 3 events and linked them

--- shop/ext/admin/controller/enquiry.php
+++ shop/ext/admin/controller/enquiry.php
@@ -1,17 +1,21 @@
 <?php
 
-class ControllerModuleEnquiry extends Controller {
-    public function index() {
-        $this->response->redirect($this->request->server['HTTP_REFERER']);
-    }
+class ControllerExtensionModuleEnquiry extends Controller {
+	public function index() {
+		$this->response->redirect($this->request->server['HTTP_REFERER']);
+	}
 
-    public function install() {
-        $this->load->model('extension/event');
-        $this->model_extension_event->addEvent('enquiry', 'catalog/controller/checkout/checkout/before', 'module/enquiry/forward');
-    }
+	public function install() {
+		$this->load->model('extension/event');
+		$this->model_extension_event->addEvent('enquiry1', 'catalog/controller/checkout/cart/after', 'extension/module/enquiry/checkout_cart_after');
+		$this->model_extension_event->addEvent('enquiry2', 'catalog/controller/checkout/checkout/before', 'extension/module/enquiry/checkout_checkout_before');
+		$this->model_extension_event->addEvent('enquiry3', 'catalog/controller/checkout/checkout/after', 'extension/module/enquiry/checkout_checkout_after');
+	}
 
-    public function uninstall() {
-        $this->load->model('extension/event');
-        $this->model_extension_event->deleteEvent('enquiry');
-    }
+	public function uninstall() {
+		$this->load->model('extension/event');
+		$this->model_extension_event->deleteEvent('enquiry1');
+		$this->model_extension_event->deleteEvent('enquiry2');
+		$this->model_extension_event->deleteEvent('enquiry3');
+	}
 }
--- shop/ext/catalog/controller/enquiry.php
+++ shop/ext/catalog/controller/enquiry.php
@@ -1,9 +1,70 @@
 <?php
 
-class ControllerModuleEnquiry extends Controller {
+class ControllerExtensionModuleEnquiry extends Controller {
 
-	public function forward(&$route, $args) {
-		 
+	public function checkout_checkout_before(&$route, $args) {
+		
+		//echo $this->session->data['currency'];
+		
+		//Shipping Address
+		$this->session->data['shipping_address']['firstname'] = '';
+		$this->session->data['shipping_address']['lastname'] = '';
+		$this->session->data['shipping_address']['company'] = '';
+		$this->session->data['shipping_address']['address_1'] = '';
+		$this->session->data['shipping_address']['address_2'] = '';
+		$this->session->data['shipping_address']['city'] = '';
+		$this->session->data['shipping_address']['postcode'] = '';
+		$this->session->data['shipping_address']['zone'] = '';
+		$this->session->data['shipping_address']['zone_id'] = '';
+		$this->session->data['shipping_address']['country'] = '';
+		$this->session->data['shipping_address']['country_id'] = '';
+		$this->session->data['shipping_address']['address_format'] = '';
+
+		//Shipping Method
+		$this->session->data['shipping_method']['title'] = '';
+		$this->session->data['shipping_method']['code'] = '';
+		
+		//Payment Address
+		$this->session->data['payment_address']['firstname'] = '';
+		$this->session->data['payment_address']['lastname'] = '';
+		$this->session->data['payment_address']['company'] = '';
+		$this->session->data['payment_address']['address_1'] = '';
+		$this->session->data['payment_address']['address_2'] = '';
+		$this->session->data['payment_address']['postcode'] = '';
+		$this->session->data['payment_address']['city'] = '';
+		$this->session->data['payment_address']['country_id'] = '';
+		$this->session->data['payment_address']['zone_id'] = '';
+		$this->session->data['payment_address']['zone'] = '';
+		$this->session->data['payment_address']['country'] = '';
+		$this->session->data['payment_address']['address_format'] = '';
+		
+		//Payment Method
+		$this->session->data['payment_method']['code'] = 'free_checkout';
+		$this->session->data['payment_method']['title'] = 'Free Checkout';
+		
+		$this->request->post['firstname'] = 'Tushar G';
+		$this->request->post['lastname'] = '';
+		$this->request->post['email'] = 'abc@abc.com';
+		$this->request->post['telephone'] = '';
+		$this->request->post['fax'] = '';
+		$this->request->post['enquiry'] = 'jkfghkdfjghkjdghjagkaj.dgnk';
+
+		//Register Customer
+		$this->load->model('account/customer');
+		$customer_id = $this->model_account_customer->addCustomer($this->request->post);
+
+		$this->session->data['account'] = 'guest';
+		
+		$this->session->data['guest']['customer_id'] = $customer_id;
+		$this->session->data['guest']['customer_group_id'] = 1;
+		$this->session->data['guest']['firstname'] = $this->request->post['firstname'];
+		$this->session->data['guest']['lastname'] = '';
+		$this->session->data['guest']['email'] = $this->request->post['email'];
+		$this->session->data['guest']['telephone'] = '';
+		$this->session->data['guest']['fax'] = '';
+		$this->session->data['guest']['custom_field'] = array();
+		$this->session->data['comment'] = $this->request->post['enquiry'];
+
 		$route = 'checkout/confirm';
 		$action = new Action($route);
 		$result = $action->execute($this->registry, array($args));
