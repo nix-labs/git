e226743145a635a46dd06e8eab921819f60176c1
developer <dev@ubuntu>
2017-03-31 22:36:36 -0400

    merged v2

--- setup-script.sh
+++ setup-script.sh
@@ -12,12 +12,13 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 ## Params
-sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password root'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
 
 ## Installations
 sudo apt-get install -y --no-install-recommends ubuntu-desktop
-sudo apt-get install -y chromium-browser apache2 mysql-server-5.6 mysql-client-5.6 php5 php5-mysql libapache2-mod-php5 php5-gd php5-mcrypt php5-curl
+sudo apt-get install -y chromium-browser default-jdk apache2 mysql-server-5.5 mysql-client-5.5 php5 php5-mysql libapache2-mod-php5 php5-gd php5-mcrypt php5-curl eclipse maven eclipse-egit mysql-workbench
+# install PDT and m2e manually
 
 ## Configs
 
@@ -41,10 +42,14 @@ echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 sudo a2enmod rewrite
 
 #MySQL
-sudo echo "DROP DATABASE IF EXISTS webshopdb" | mysql -uroot -proot
 sudo echo "CREATE USER 'dbadmin'@'localhost' IDENTIFIED BY 'dbadmin'" | mysql -uroot -proot
+sudo echo "DROP DATABASE IF EXISTS webshopdb" | mysql -uroot -proot
+sudo echo "DROP DATABASE IF EXISTS marsdb" | mysql -uroot -proot
 sudo echo "CREATE DATABASE webshopdb" | mysql -uroot -proot
+sudo echo "CREATE DATABASE marsdb" | mysql -uroot -proot
 sudo echo "GRANT ALL PRIVILEGES ON webshopdb.* TO 'dbadmin'@'localhost'" | mysql -uroot -proot
+sudo echo "GRANT ALL PRIVILEGES ON marsdb.* TO 'dbadmin'@'localhost'" | mysql -uroot -proot
+
 sudo echo "FLUSH PRIVILEGES" | mysql -uroot -proot
 
 #PHP
@@ -65,5 +70,18 @@ sudo find /home/dev/webroot -type f -exec chmod 0644 {} \;
 sudo find /home/dev/git -type d -exec chmod 2750 {} \;
 sudo find /home/dev/git -type f -exec chmod 0644 {} \;
 sudo sed -i 's/www-data/dev/g' /etc/apache2/envvars
+
+ln -s /home/dev/webroot/shop/ext/admin/controller/enquiry.php /home/dev/webroot/shop/admin/controller/extension/module/enquiry.php
+ln -s /home/dev/webroot/shop/ext/admin/language/enquiry.php /home/dev/webroot/shop/admin/language/en-gb/extension/module/enquiry.php
+ln -s /home/dev/webroot/shop/ext/catalog/controller/enquiry.php /home/dev/webroot/shop/catalog/controller/extension/module/enquiry.php
+
+ln -s /home/dev/webroot/shop/ext/catalog/controller/contact.php /home/dev/webroot/shop/catalog/controller/extension/total/contact.php
+ln -s /home/dev/webroot/shop/ext/catalog/language/contact.php /home/dev/webroot/shop/catalog/language/en-gb/extension/total/contact.php
+ln -s /home/dev/webroot/shop/ext/catalog/view/contact.tpl /home/dev/webroot/shop/catalog/view/theme/default/template/extension/total/contact.tpl
+
+ln -s /home/dev/webroot/shop/ext/catalog/view/xheader.tpl /home/dev/webroot/shop/catalog/view/theme/default/template/common/xheader.tpl
+ln -s /home/dev/webroot/shop/ext/catalog/view/xfooter.tpl /home/dev/webroot/shop/catalog/view/theme/default/template/common/xfooter.tpl
+ln -s /home/dev/webroot/shop/ext/catalog/language/xheader.php /home/dev/webroot/shop/catalog/language/en-gb/common/xheader.php
+ln -s /home/dev/webroot/shop/ext/catalog/language/xfooter.php /home/dev/webroot/shop/catalog/language/en-gb/common/xfooter.php
 
 sudo reboot
