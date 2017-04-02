d37ffd11e7d925d4ed020eb5bbb7d07c7d71effd
nix-labs <tushargupta2809@gmail.com>
2017-04-01 23:51:09 -0400

    added mvn script

--- setup-script-v2.sh
+++ setup-script-v2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #sudo apt-get install -y 
+cd /home/dev/git
+mvn archetype:generate -DgroupId=com.nix.mars -DartifactId=jshop -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+cd /home/dev/git/jshop
+mvn eclipse:eclipse -Dwtpversion=2.0
 
 #oc cli
 #launcher
