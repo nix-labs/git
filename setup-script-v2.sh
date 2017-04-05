e1fbcf2d1452a02f124cc4f1ca6ce6e0ab4b8954
developer <dev@ubuntu>
2017-04-05 14:26:18 -0400

    updated mvn

--- setup-script-v2.sh
+++ setup-script-v2.sh
@@ -3,8 +3,6 @@
 #sudo apt-get install -y bleachbit synaptic
 #cd /home/dev/git
 #mvn archetype:generate -DgroupId=com.nix.mars -DartifactId=jshop -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
-#cd /home/dev/git/jshop
-#mvn eclipse:eclipse -Dwtpversion=2.0
 
 #oc cli
 #launcher
