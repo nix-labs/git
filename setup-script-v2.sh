ad3300601a722d12f288f7abe191e10a867086e1
nix-labs <tushargupta2809@gmail.com>
2017-04-05 13:23:36 -0400

    added bleachbit synaptic

--- setup-script-v2.sh
+++ setup-script-v2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#sudo apt-get install -y 
-cd /home/dev/git
-mvn archetype:generate -DgroupId=com.nix.mars -DartifactId=jshop -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
-cd /home/dev/git/jshop
-mvn eclipse:eclipse -Dwtpversion=2.0
+#
+#sudo apt-get install -y bleachbit synaptic
+#cd /home/dev/git
+#mvn archetype:generate -DgroupId=com.nix.mars -DartifactId=jshop -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+#cd /home/dev/git/jshop
+#mvn eclipse:eclipse -Dwtpversion=2.0
 
 #oc cli
 #launcher
