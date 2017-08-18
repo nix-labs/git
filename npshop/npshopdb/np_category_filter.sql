6eccee3225be75698ffdfa07048d07eec87d7069
Tushar Gupta <tgupta@tushargupta.com>
2017-08-17 23:38:23 -0400

    consolidated sql

--- npshop/npshopdb/np_category_filter.sql
+++
@@ -1,9 +0,0 @@
-USE npshopdb;
-
-CREATE TABLE `np_category_filter` (
-  `category_id` int(11) NOT NULL,
-  `filter_id` int(11) NOT NULL,
-  PRIMARY KEY (`category_id`,`filter_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
