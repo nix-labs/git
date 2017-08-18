0c77b21ec365fed5244c620557d46b0d52bfbbdc
Tushar Gupta <tushargupta2809@gmail.com>
2017-08-17 23:41:00 -0400

    add export script

--- npshop/npshopdb/execute_sql.sql
+++ npshop/npshopdb/execute_sql.sql
@@ -2,3 +2,6 @@ CREATE DATABASE npshopdb;
 CREATE USER 'tgadmin'@'localhost' IDENTIFIED BY 'passw0rd';
 GRANT ALL PRIVILEGES ON npshopdb.* TO 'tgadmin'@'localhost';
 FLUSH PRIVILEGES;
+
+mysqldump --add-drop-database -u tgadmin -p --databases npshopdb > npshopdb.sql
+mysql -u tgadmin -p npshopdb < npshopdb.sql
