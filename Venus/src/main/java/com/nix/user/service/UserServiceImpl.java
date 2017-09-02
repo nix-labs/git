b688ce6e33c69a1cc3e9fba8459fffd8e32181d8
Tushar Gupta <tushar_gupta@abercrombie.com>
2017-09-02 16:06:40 -0400

    added venus source

---
+++ Venus/src/main/java/com/nix/user/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.nix.user.service;
+
+import java.util.List;
+
+import com.nix.user.dao.UserDao;
+import com.nix.user.model.User;
+
+public class UserServiceImpl implements UserService {
+
+	private UserDao userDao;
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	public List<User> getAllUsers() {
+		return userDao.getAllUsers();
+	}
+
+	public User findByUserId(long id) {
+		return userDao.findByUserId(id);
+	}
+
+	public User findByUsername(String username) {
+		return userDao.findByUsername(username);
+	}
+
+	public User createUser(User user) {
+		if(findByUsername(user.getUsername()) != null) {
+			return null;
+		}
+		return userDao.createUser(user);
+	}
+
+	public User updateUser(long id, User user) {
+		if(findByUserId(id) == null) {
+			return null;
+		}
+		return userDao.updateUser(user);
+	}
+
+	public User deleteUser(long id) {
+		if(findByUserId(id) == null) {
+			return null;
+		}
+		return userDao.deleteUser(id);
+	}
+}
