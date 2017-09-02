20be79f09c2b7306234b726372db1ad518c6640b
developer <dev@ubuntu>
2017-09-02 19:11:21 -0400

    moved setup scripts

---
+++ setup-vm/setup-init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Init
+wget https://goo.gl/z3uKuj -O setup-vm.sh
+sudo chmod 777 setup-vm.sh
+sudo ./setup-vm.sh
---
+++ setup-vm/setup-ws.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Ubuntu
+sudo xrandr -s 1440x900
+
+#npshop
+sudo cp -a /home/dev/git/npshop/. /home/dev/webroot/npshop
+mysql -u root -proot < /home/dev/git/npshop/npshopdb/npshopdb.sql
+sudo echo "GRANT ALL PRIVILEGES ON npshopdb.* TO 'tgadmin'@'localhost'" | mysql -uroot -proot
+
+#shadi
+sudo cp -a /home/dev/git/shadi/. /home/dev/webroot/shadi
+mysql -u root -proot < /home/dev/git/shadi/rsvp.sql
+sudo echo "GRANT ALL PRIVILEGES ON shadidb.* TO 'tgadmin'@'localhost'" | mysql -uroot -proot
+
+#venus
+cd /opt/dev/git/Venus
+mvn eclipse:eclipse
+mysql -u root -proot < /home/dev/git/Venus/sql/venus.sql
+sudo echo "GRANT ALL PRIVILEGES ON venusdb.* TO 'tgadmin'@'localhost'" | mysql -uroot -proot
+
+sudo echo "FLUSH PRIVILEGES" | mysql -uroot -proot
+
+
+#tomcat config
+#cd ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/    
+#rm org.eclipse.jst.server.tomcat.core.prefs    
+#rm org.eclipse.wst.server.core.prefs
+
+sudo service tomcat7 stop
+sudo update-rc.d tomcat7 disable
+
+cd /usr/share/tomcat7
+sudo ln -s /var/lib/tomcat7/conf conf
+sudo ln -s /var/lib/tomcat7/common common
+sudo ln -s /var/lib/tomcat7/server server
+sudo ln -s /var/lib/tomcat7/shared shared
+sudo ln -s /etc/tomcat7/policy.d/03catalina.policy conf/catalina.policy
+sudo ln -s /var/log/tomcat7 log
+sudo chmod -R 777 /usr/share/tomcat7/conf
+
