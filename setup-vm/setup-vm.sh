67db37390c0bc1c9c21b67ab390712f9aa8cca9f
developer <dev@ubuntu>
2017-10-01 18:36:02 -0400

    removed tomcat

--- setup-vm/setup-vm.sh
+++ setup-vm/setup-vm.sh
@@ -11,7 +11,7 @@ sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_aga
 
 ## Installations
 sudo apt-get install -y --no-install-recommends ubuntu-desktop
-sudo apt-get install -y default-jdk firefox apache2 mysql-server-5.5 mysql-client-5.5 php5 php5-mysql libapache2-mod-php5 php5-gd php5-mcrypt php5-curl mysql-workbench git maven tomcat7 open-vm-tools-lts-trusty open-vm-tools-lts-trusty-desktop
+sudo apt-get install -y default-jdk firefox apache2 mysql-server-5.5 mysql-client-5.5 php5 php5-mysql libapache2-mod-php5 php5-gd php5-mcrypt php5-curl mysql-workbench eclipse git maven open-vm-tools-lts-trusty open-vm-tools-lts-trusty-desktop
 
 ## Configs
 
@@ -33,32 +33,12 @@ sudo git config --global user.name "developer"
 sudo git clone https://github.com/nix-labs/git.git
 
 ## Permissions
-sudo chown -R dev:dev /home/dev
+#sudo chown -R dev:dev /home/dev
 sudo sed -i 's/www-data/dev/g' /etc/apache2/envvars
-sudo find /home/dev/git -type d -exec chmod 2750 {} \;
-sudo find /home/dev/git -type f -exec chmod 0644 {} \;
+#sudo find /home/dev/git -type d -exec chmod 2750 {} \;
+#sudo find /home/dev/git -type f -exec chmod 0644 {} \;
 
 sudo cp -a /home/dev/git/setup-vm/000-default.conf /etc/apache2/sites-available/000-default.conf
-
-#eclipse
-wget http://mirror.cc.vt.edu/pub/eclipse/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
-sudo tar -xvzf eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz -C /opt
-sudo rm eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
-sudo cp /home/dev/git/setup-vm/eclipse.desktop /usr/share/applications/eclipse.desktop
-sudo desktop-file-install /usr/share/applications/eclipse.desktop
-sudo ln -s /opt/eclipse/eclipse /usr/local/bin
-
-#Tomcat
-sudo service tomcat7 stop
-sudo update-rc.d tomcat7 disable
-cd /usr/share/tomcat7
-sudo ln -s /var/lib/tomcat7/conf conf
-sudo ln -s /var/lib/tomcat7/common common
-sudo ln -s /var/lib/tomcat7/server server
-sudo ln -s /var/lib/tomcat7/shared shared
-sudo ln -s /etc/tomcat7/policy.d/03catalina.policy conf/catalina.policy
-sudo ln -s /var/log/tomcat7 log
-sudo chmod -R 777 /usr/share/tomcat7/conf
 
 #Cleanup
 sudo apt-get -f install
--- setup-vm/setup-ws.sh
+++ setup-vm/setup-ws.sh
@@ -22,9 +22,32 @@ sudo echo "GRANT ALL PRIVILEGES ON venusdb.* TO 'tgadmin'@'localhost'" | mysql -
 
 sudo echo "FLUSH PRIVILEGES" | mysql -uroot -proot
 
+#sudo find /home/dev/git -type d -exec chmod 2750 {} \;
+#sudo find /home/dev/git -type f -exec chmod 0644 {} \;
 
+#Backups
+#sudo apt-get install -y tomcat7
+
+#eclipse
+#wget http://mirror.cc.vt.edu/pub/eclipse/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
+#sudo tar -xvzf eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz -C /opt
+#sudo rm eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
+#sudo cp /home/dev/git/setup-vm/eclipse.desktop /usr/share/applications/eclipse.desktop
+#sudo desktop-file-install /usr/share/applications/eclipse.desktop
+#sudo ln -s /opt/eclipse/eclipse /usr/local/bin
+
+#Tomcat
+#sudo service tomcat7 stop
+#sudo update-rc.d tomcat7 disable
+#cd /usr/share/tomcat7
+#sudo ln -s /var/lib/tomcat7/conf conf
+#sudo ln -s /var/lib/tomcat7/common common
+#sudo ln -s /var/lib/tomcat7/server server
+#sudo ln -s /var/lib/tomcat7/shared shared
+#sudo ln -s /etc/tomcat7/policy.d/03catalina.policy conf/catalina.policy
+#sudo ln -s /var/log/tomcat7 log
+#sudo chmod -R 777 /usr/share/tomcat7/conf
 #tomcat config
 #cd ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/    
 #rm org.eclipse.jst.server.tomcat.core.prefs    
 #rm org.eclipse.wst.server.core.prefs
-
