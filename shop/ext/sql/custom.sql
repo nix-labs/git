607f6939f7f19a2d9592424c8d90d594e81785f2
Tushar Gupta <tgupta@tushargupta.com>
2017-10-31 21:13:21 -0400

    removed shop

--- shop/ext/sql/custom.sql
+++
@@ -1,7 +0,0 @@
-update setting set value='0' where code='coupon' and `key`='coupon_status';
-update setting set value='0' where code='voucher' and `key`='voucher_status';
-update setting set value='0' where code='tax' and `key`='tax_status';
-update setting set value='0' where code='sub_total' and `key`='sub_total_status';
-update setting set value='0' where code='shipping' and `key`='shipping_status';
-
-insert into extension values (33,'total', 'contact');
