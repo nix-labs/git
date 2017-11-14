8f393a050b8e1a23b67aa4f6b970168aa22d4e55
Tushar Gupta <tgupta@tushargupta.com>
2017-11-13 23:28:53 -0500

    oc ext

---
+++ setup-vm/ProductImages/upload/admin/language/english/module/ordprdimg.php
@@ -0,0 +1,16 @@
+<?php
+// Heading
+$_['heading_title']    = 'Product Image In Order Confirmation Email';
+$_['doc_title']    = 'Product Image In Order Confirmation Email';
+
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified Product Image In Order Confirmation Email module!';
+$_['text_edit']        = 'Edit product Image In Order Confirmation Email Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_image_size']     = 'Image Size';
+$_['entry_image_col_name']     = 'Image Column Name';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Product Image In Order Confirmation Email module!';
