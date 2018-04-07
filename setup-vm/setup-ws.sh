8dc9fbd88aa201a316cbac8a9e96c7f13b1b5e81
Tushar Gupta <tushargupta2809@gmail.com>
2018-04-06 21:01:00 -0400

    cleaned-up comments

--- setup-vm/setup-ws.sh
+++ setup-vm/setup-ws.sh
@@ -8,11 +8,6 @@ cp -a /home/dev/git/npshop/. /home/dev/webroot/npshop
 mysql -u root -proot < /home/dev/git/npshop/npshopdb/npshopdb.sql
 echo "GRANT ALL PRIVILEGES ON npshopdb.* TO 'tgadmin'@'localhost'" | mysql -uroot -proot
 
-#shop
-cp -a /home/dev/git/shop/. /home/dev/webroot/shop
-mysql -u root -proot < /home/dev/git/shop/shopdb.sql
-echo "GRANT ALL PRIVILEGES ON shopdb.* TO 'tgadmin'@'localhost'" | mysql -uroot -proot
-
 #shadi
 cp -a /home/dev/git/shadi/. /home/dev/webroot/shadi
 mysql -u root -proot < /home/dev/git/shadi/rsvp.sql
@@ -29,30 +24,3 @@ echo "FLUSH PRIVILEGES" | mysql -uroot -proot
 
 find /home/dev/git -type d -exec chmod 2750 {} \;
 find /home/dev/git -type f -exec chmod 0644 {} \;
-
-#Backups
-#sudo apt-get install -y tomcat7
-
-#eclipse
-#wget http://mirror.cc.vt.edu/pub/eclipse/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
-#sudo tar -xvzf eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz -C /opt
-#sudo rm eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
-#sudo cp /home/dev/git/setup-vm/eclipse.desktop /usr/share/applications/eclipse.desktop
-#sudo desktop-file-install /usr/share/applications/eclipse.desktop
-#sudo ln -s /opt/eclipse/eclipse /usr/local/bin
-
-#Tomcat
-#sudo service tomcat7 stop
-#sudo update-rc.d tomcat7 disable
-#cd /usr/share/tomcat7
-#sudo ln -s /var/lib/tomcat7/conf conf
-#sudo ln -s /var/lib/tomcat7/common common
-#sudo ln -s /var/lib/tomcat7/server server
-#sudo ln -s /var/lib/tomcat7/shared shared
-#sudo ln -s /etc/tomcat7/policy.d/03catalina.policy conf/catalina.policy
-#sudo ln -s /var/log/tomcat7 log
-#sudo chmod -R 777 /usr/share/tomcat7/conf
-#tomcat config
-#cd ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/    
-#rm org.eclipse.jst.server.tomcat.core.prefs    
-#rm org.eclipse.wst.server.core.prefs
